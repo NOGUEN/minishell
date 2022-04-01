e457b8710dce9a3ed26d0f8e6b86b28aecedeb67
NOGUEN <nhg1113@naver.com>
2022-04-01 12:19:34 +0900

    signal & signal handler(not complete. skeleton code)

--- include/minishell.h
+++ include/minishell.h
@@ -10,6 +10,18 @@
 #include <readline/history.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
+#include "libft/libft.h"
 
+#define PIPE    0000000001
+#define EXIT    0000000010
+#define RIGHT   0000000100
+#define REDIR   0000001000
+
+/* signal */
+void    signal_init(void);
+
+/* check */
+int     check_whitespace(char *line);
 
 #endif
