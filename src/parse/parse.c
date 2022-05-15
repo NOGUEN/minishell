bfec916187c331d1cdfadf9b85adb6d91ae905cb
hnoh <hnoh@student.42seoul.kr>
2022-05-15 17:26:59 +0900

    fix parsing

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,31 +6,38 @@
 /*   By: noguen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 00:44:35 by noguen            #+#    #+#             */
-/*   Updated: 2022/05/15 14:38:28 by hnoh             ###   ########.fr       */
+/*   Updated: 2022/05/15 17:26:10 by hnoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../include/parse.h"
 
-void	parse_set(int *index, int *begin, int *flag, char *line)
+void	parse_set(int *index, int *begin, int *flag)
 {
 	*index = 0;
 	*begin = 0;
 	*flag = PIPE;
-	if ((line[*index] == '\"' || line[*index] == '\'') && (*flag & QUOTE) == 0)
+}
+
+void	parse_set2(int index, int *flag, char *line)
+{
+	if ((line[index] == '\"' || line[index] == '\'') && ((*flag) & QUOTE) == 0)
 		(*flag) |= QUOTE;
-	else if ((line[*index] == '\"' || line[*index] == '\'')
-		&& (*flag & PIPE) != 0)
+	else if ((line[index] == '\"' || line[index] == '\'')
+		&& (*flag & QUOTE) != 0)
 		(*flag) &= !QUOTE;
 }
 
 void	parse_split1(char *line, int index, int *flag)
 {
 	if (line[index] == '|')
+	{
 		line[index] = '\0';
+	}
 	else
-		*flag &= !PIPE;
+		(*flag) &= !PIPE;
+
 }
 
 void	parse_split2(t_cmd **cmds, t_cmd **tmp, t_cmd *new, int begin)
@@ -55,9 +62,10 @@ void	parse(t_cmd **cmds, char *line, char **envp)
 	t_cmd	*new;
 	t_cmd	*tmp;
 
-	parse_set(&index, &begin, &flag, line);
+	parse_set(&index, &begin, &flag);
 	while (1)
 	{
+		parse_set2(index, &flag, line);
 		if (line[index] == '\0' || (line[index] == '|' && (flag & QUOTE) == 0))
 		{
 			parse_split1(line, index, &flag);
